01b4b01f22c6798ff11b9a598fc254d6e80b00d8
agippos <agippos@gmail.com>
2018-07-22 20:00:44 -0700

    initial

---
+++ client/app/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css';
+
+
+const Header = props => (
+  <header className="header-banner">
+  <div>
+    <h1>Aqua-Zotic</h1>
+    <h2>
+      Arizona's premier exotic fish store!
+    </h2>
+    
+    </div>
+  </header>
+);
+
+export default Header;
